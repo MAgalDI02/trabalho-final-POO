34689825961250b3757aae0ff5f2344a319867ea
Magaldi <lucaslutador10@gmail.com>
2023-11-01 08:43:11 -0300

    1.0

---
+++ demo/src/main/java/com/example/demo/model/user.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+
+
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String user_name;
+    private String password;
+    private String email;
+    
+
+
+}
